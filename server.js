bfbf45d4d6242c236b9e73b9ea1daff1ca0189a4
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-02-07 13:16:24 -0500

    adding post req 4 name and passowrd

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
 const express = require('express');
 const app = express();
+var bcrypt = require('bcrypt');
 
 app.use(express.json())
 
-const users = [{
-    name: 'admin'
-}];
+const users = [];
 
 app.get('/users', (req, res) => {
     res.json(users);
 });
+
 app.post('/users', (req, res) => {
-    const user = {
-        name: req.body.name,
-        password: req.body.password
-    }
-    users.push(user)
-    res.status(201).send()
 
+    async(req, res) => {
+        try {
+             const salt = await bcrypt.genSalt()
+                 const hashedPassword = await bcrypt.hash(req.body.password, salt)
+                 console.log(salt)
+                 console.log(hashedPassword)                 
+                const user = { name: req.body.name, password: hashedPassword }
+                users.push(user)
+                res.status(201).send()
+                 } catch {
+                     res.status(500).send()
+                 }}
 });
-
-
-
-
-
-
 app.listen(3000);
 
 
